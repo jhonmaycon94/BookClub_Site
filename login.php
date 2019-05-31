e8ce2dd8c5fd61ac7d9535f5b8a054f91a22ebfd
jhon maycon <jhonmaycon94@gmail.com>
2019-05-31 10:45:00 -0300

    deixando a interface mais modular

--- login.php
+++ login.php
@@ -1,61 +1,20 @@
-<!DOCTYPE html>
-<html lang="pt-BR"/>
-  <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, user-scalable=no"/>
-    <title>Página Inicial</title>
-    <link rel="stylesheet" href="_css/form.css"/>
-    <link rel="stylesheet" href="_css/estilo.css"/>
-    <link rel="stylesheet" href="_css/login.css"/>
-    <link href="https://fonts.googleapis.com/css?family=East+Sea+Dokdo|Pacifico&display=swap" rel="stylesheet"/>
-    <script src="_js/validaLogin.js"></script>
-  </head>
-  <body>
-    <!--cabeçalho-->
-    <header id="cabecalho">
-      <h1>Book Club Estância</h1>
-      <nav id="nav_menu">
-      </nav>
-    </header>
-    <div id="corpo">
-      <!--formulario-->
-        <div id="form">
-          <form action="processaLogin.php" method="post">
-              <div class="username_field">
-                <label for="username">Usuário:</label><br/>
-                <input type="text" name="username" id="username" value="" size="20">
-              </div>
-                <div class="senha_field">
-                  <label for="senha">Senha:</label><br/>
-                  <input type="password" name="senha" id="senha" size="20" maxlength="8">
-                </div>
-                <div class="bt_login_field">
-                  <button class="classname" type="submit" name="bt_login" id="bt_login" onclick="return validaLogin()">Login</button>
-                </div>
-            </form>
-          </div>
+<?php include('includes/overall/overallHeader.php');?>
+
+<!--formulario-->
+<div id="form">
+<form action="processaLogin.php" method="post">
+  <div class="username_field">
+    <label for="username">Usuário:</label><br/>
+    <input type="text" name="username" id="username" value="" size="20">
+  </div>
+    <div class="senha_field">
+      <label for="senha">Senha:</label><br/>
+      <input type="password" name="senha" id="senha" size="20" maxlength="8">
     </div>
-    <footer id="rodape">
-      <p>&copy Jhon Maycon 2019</p>
-    </footer>
-    <?php
+    <div class="bt_login_field">
+      <button class="classname" type="submit" name="bt_login" id="bt_login" onclick="return validaLogin()">Login</button>
+    </div>
+</form>
+</div>
 
-    require('conexao.php');
-
-    $cliente_usuario = filter_input(INPUT_POST, 'username');
-    $cliente_senha = filter_input(INPUT_POST, 'senha');
-
-    if ($result = $conn->query("SELECT * FROM usuarios WHERE username = '$cliente_usuario' AND senha = '$cliente_senha'")) {
-          if($result->num_rows>0){
-            echo "cliente logado";
-          }else{
-          echo "cliente não logado";
-        }
-      }
-
-        /* free result set */
-        $result->close();
-        $conn->close();
-    ?>
-</body>
-</html>
+<?php include('includes/overall/overallFooter.php');?>
