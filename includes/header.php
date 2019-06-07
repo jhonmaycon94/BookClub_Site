cc0bb1926be02e669e88b9cd2c5eef22987cb815
Jhon Maycon <jhon.maycon@hotmail.com>
2019-06-07 15:55:11 -0300

    .

--- includes/header.php
+++ includes/header.php
@@ -2,5 +2,9 @@
  <header id="cabecalho">
     <h1>Book Club Estância</h1>
     <nav id="nav_menu">
+    	<ul id="menu">
+    		<li><a href = "login.php">Login</a></li>
+    		<li><a href = "cadastro.php">Cadastro</a></li>
+    	</ul>
     </nav>
 </header>
--- index.php
+++
@@ -1,8 +0,0 @@
-<?php include('includes/overall/overallHeader.php');?>
-
-<p>O Book Club é uma iniciativa dos<br/> alunos do Instituto Federal de Sergipe<br/> para estimular a leitura!</p>
-<img src="_imagens/logo_bookclub.jpg" alt="logo do book club"/>
-<a href="login.php" class="classname" id="bt_login">LOGIN</a>
-<a href="cadastro.php" class="classname" id="bt_entre_clube">PARTICIPE</a>
-
-<?php include('includes/overall/overallFooter.php');?>    
